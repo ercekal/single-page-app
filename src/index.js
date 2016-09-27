58424d1a115a69fb38710fcc2fd3cfbbbce042bf
ercekal <ercekalabalikoglu@gmail.com>
2016-09-27 16:09:22 +0100

    each item is collected but not able to list below

--- src/index.js
+++ src/index.js
@@ -13,9 +13,25 @@ function ready(fn) {
 
 ready(function(){
   // use JS to listen to form text input and submit
+  var list = new List();
+
   document.getElementById('create_note').onclick = function(){
     var text = document.getElementById('notes_input').value;
     var note = new Note(text);
-    note.save();
+    list.save(note);
+    console.log(list);
+    listNotes();
+    // document.getElementById("listing").innerHTML = list.notes[0].text;
   };
+
+
+
+  function listNotes() {
+      for (i=0; i<(list.notes.length); i++)
+      var t = list.notes[i].text;
+    {
+      document.getElementById("listing").innerHTML = t;
+      console.log(list.notes);
+    }
+  }
 });
