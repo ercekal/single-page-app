c8abb51a0b775180ab6622d1ab0bd9e1cd4d6cb5
ercekal <ercekalabalikoglu@gmail.com>
2016-09-27 11:05:56 +0100

    test framework is being developed

---
+++ test/aubergine.js
@@ -0,0 +1,11 @@
+var assert = {
+ toEqual: function(actual, expectation) {
+   if(actual != expectation) {
+     throw new Error("Expected " + actual + " to equal " + expectation);
+   } else {
+     console.log(actual + " is equal to " + expectation);
+   }
+ }
+};
+
+module.export = assert;
