28872b1ad6d14e00abb088a0f2c776880c183cb3
Rosie Allott <rosie.allott@gmail.com>
2016-09-28 16:57:05 +0100

    Aubergine test functions

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,10 +1,10 @@
-
-
-describe("note", function(){
-
-
+describe("Note class ", function(){
   it("should contain note text", function(){
     var note = new Note("test");
     expect(note.text).toEqual("test");
+  });
+  it("should return the note if #show is called", function(){
+    var note = new Note("test");
+    expect(note.show()).toEqual(note);
   });
 });
--- testframework/aubergine.js
+++ testframework/aubergine.js
@@ -8,21 +8,23 @@ var TestObject = function TestObject(actual){
 
 TestObject.prototype =  {
   toEqual: function(expectation) {
-    if(this.actual != expectation) {
+    if(this.actual !== expectation) {
       throw new Error("Expected " + this.actual + " to equal " + expectation);
     } else {
-      console.log("passed:" + this.actual + " is equal to " + expectation);
+      console.log("      Passed:" + this.actual + " is equal to " + expectation);
     }
   }
 };
 
 
 function it(string, callback){
-  console.log(string);
+  console.log("  " + string);
   callback();
 }
 function describe(string, callback){
   console.log(string);
   callback();
 }
-test
+function beforeEach(callback){
+  callback();
+}
