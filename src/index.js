7dfedab8cd85a054c10462689368b79ccf560745
ercekal <ercekalabalikoglu@gmail.com>
2016-09-28 16:52:20 +0100

    managed to add target and id to each list item

--- src/index.js
+++ src/index.js
@@ -29,9 +29,24 @@ ready(function(){
       var newItem = document.createElement("li");
       var a = document.createElement("a");
       var short = (list.notes.slice(-1)[0].abbreviated);
-      a.textContent = short;
+      a.textContent = short + "...";
       a.setAttribute('href', "#");
+      newItem.setAttribute('id', 'notes_list');
+      newItem.setAttribute('target', (ulist.children.length));
       newItem.appendChild(a);
       ulist.appendChild(newItem);
+      // a = document.getElementsByTagName("li");
+      // console.log(a);
   }
+  // jQuery(function(){
+  //   jQuery('notes_list').click(function(){
+  //     console.log($this.attr('target'));
+  //
+  //   });
+
+  //
+  // document.getElementsByTagName("notes_list").onclick = function() {
+  //   var attribute = Element.getAttribute("target");
+  //   console.log(attribute);
+  // };
 });
