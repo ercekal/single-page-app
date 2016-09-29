b889b9d791f339949dace319bbfa58cfb491b46b
Ewan Sheldon <ewansheldon@gmail.com>
2016-09-29 12:39:29 +0100

    full note view and back button and prettty much everything else

--- src/index.js
+++ src/index.js
@@ -32,19 +32,41 @@ ready(function(){
      var a = document.createElement("a");
      var short = (list.notes.slice(-1)[0].abbreviated);
      var index = ulist.children.length;
+     var backButton = document.getElementById("back_button");
+     console.log(backButton);
      a.textContent = short + "...";
      a.setAttribute('href', "#");
      newItem.setAttribute('id', 'notes_list');
      newItem.setAttribute('target', index);
      newItem.appendChild(a);
      ulist.appendChild(newItem);
+     var fullNote = list.notes[index].text;
+     var paragraph = document.createElement("p");
+     paragraph.setAttribute('id', index);
+     paragraph.setAttribute('style', 'display: none');
+     var viewDiv = document.getElementById("single_view");
+     paragraph.textContent = fullNote;
+     viewDiv.appendChild(paragraph);
      addLiListener(newItem, index);
+     returnBack(index);
  }
 
+ function addLiListener(newItem, index) {
+   newItem.addEventListener('click', function(){
+     document.getElementById("listing").style.display = 'none';
+     document.getElementById(index).style.display = 'inline';
+     document.getElementById('back_button').style.display = 'inline';
+   });
+ }
 
- document.getElementById("list").onclick = function() {
-   document.getElementById("single_view").innerHTML = list.notes[0].text;
- };
-
+function returnBack(index){
+  document.getElementById('back_button').addEventListener('click', function(){
+    document.getElementById("listing").style.display = 'inline';
+    document.getElementById(index).style.display = 'none';
+    document.getElementById('back_button').style.display = 'none';
 
 });
+ }
+
+ }
+);
